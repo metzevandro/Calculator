80270e2ba82090f3eed64167df572c4ea09feba2
metzevandro <metzxevandro@gmail.com>
2023-09-06 18:48:39 -0300

    adicionado estilos a calculadora

--- src/app/page.js
+++ src/app/page.js
@@ -52,7 +52,7 @@ export default function Home() {
   return (
     <section className="flex items-center justify-center h-screen">
       <div className="flex flex-col rounded-[40px] items-center justify-center w-[350px] h-[600px] bg-black shadow-2xl">
-        <div className="flex text-4xl w-[85%] h-[15%] flex-row text-white justify-end items-end mb-[15px]">
+        <div className="bg-gray p-4 rounded-2xl flex text-4xl w-[85%] h-[15%] flex-row text-black justify-end items-end mb-[15px]">
           {result || input}
         </div>
         <div className="w-[85%] flex flex-col gap-5 text-white">
